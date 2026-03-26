f78d262c526d0715ae36871f619a301f5149fff1
henry28312701 <henry28312701@gmail.com>
2026-03-26 10:13:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,36 @@
+const CACHE_NAME = 'guitar-cache-v2';
+
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('guitar-cache').then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(['/']);
     })
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
